974e4eea1430448e9acd9ce7649acf46d468e7b5
lobatolc <lucaslobto@hotmail.com>
2023-06-14 13:31:27 -0300

    Initial Configs

---
+++ src/Components/Box/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #EEF2F6;
+  padding: 1rem;
+`;
---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Home() {
+  return(
+    <Container>
+        
+    </Container>
+  );
+}
+
+export default Home;
